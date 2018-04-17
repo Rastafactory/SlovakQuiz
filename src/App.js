3bcf20f3fb807ed4353cb643d046594070f65983
milos kruk <milos@thinkcreatix.com>
2018-04-17 22:16:03 +0200

    Log in formular

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
     render() {
         return(
-            <View>
+            <View >
+                <Header headerText="Authentication"/>
                 <Text>SlovakQuiz</Text>
+                <LoginForm/>
             </View>
         );
     }
